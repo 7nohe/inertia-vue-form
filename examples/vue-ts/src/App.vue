4fdb73eb31c939bb54f15c88105bf93cf46ce442
7nohe <7nohe.urata@gmail.com>
2023-04-27 00:39:23 +0900

    initial commit

---
+++ examples/vue-ts/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <div>
+    <Form />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ examples/vue-ts/src/components/Form.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useZodForm } from '@inertia-vue-form/zod';
+import { z } from 'zod';
+
+const schema = z.object({
+  name: z.string().max(10),
+  email: z.string().email().max(255).optional(),
+})
+
+const form = useZodForm({
+  name: '',
+  email: '',
+}, schema);
+
+function submit() {
+  form.post('http://api.example.com')
+}
+</script>
+
+<template>
+  <form @submit.prevent="submit">
+    <label for="name">Name</label>
+    <br>
+    <input name="name" v-model="form.name" />
+    <p v-show="form.errors.name" style="color: red;">{{ form.errors.name }}</p>
+    <br>
+    <label for="email">Email</label>
+    <br>
+    <input name="email" v-model="form.email" />
+    <p v-show="form.errors.email" style="color: red;">{{ form.errors.email }}</p>
+    <br>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
